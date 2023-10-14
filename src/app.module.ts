a63f42e6a18c45f2b1a522a88a431f8147b16a16
YusufNasar <yusuf.nasar@twerlo.com>
2023-10-14 21:22:20 +0300

    feat: init auth and employee modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EmployeeModule } from './employee/employee.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 
 @Module({
   imports: [
+    AuthModule,
+    EmployeeModule,
     ConfigModule.forRoot({
       envFilePath: '.env.dev',
       load: [configuration],
