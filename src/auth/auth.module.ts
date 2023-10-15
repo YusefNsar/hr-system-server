84c19116f132b55b8d932348ae70abc140c57e6d
YusufNasar <yusuf.nasar@twerlo.com>
2023-10-15 03:29:21 +0300

    chore: setup jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,33 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { EmployeeModule } from 'src/employee/employee.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
-@Module({})
+@Module({
+  imports: [
+    // setup jwt
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: '24h' },
+      }),
+      inject: [ConfigService],
+    }),
+    // auth-related modules
+    EmployeeModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+})
 export class AuthModule {}
